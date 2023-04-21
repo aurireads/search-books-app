9354c4e7baa4de13a0bafd7d193060ec1d20c7d1
aurireads <comealongmari@gmail.com>
2023-04-21 20:08:00 -0300

    Adição de Navbar e algumas alterações em CSS.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className='holder'>
             <header className='header'>
                 <Navbar />
-                <div className='header-content flex flex-c text center text white'>
+                <div className='header-content flex flex-c text center text-white'>
                     <h2 className='header-title text-capitalize'>find your book</h2><br />
                     <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere quas magnam iste similique temporibus provident hic dicta culpa voluptas? Commodi quidem, quas maiores possimus libero porro cum officia corporis nemo.</p>
                     <SearchForm />
